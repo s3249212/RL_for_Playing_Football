0040120b41363e76c433a6094bf7df1131a279d9
Julian <julianhegeman@gmail.com>
2019-06-12 15:19:38 +0200

    Moved softmax function aroudn :

--- BP/Util/util_functions.cpp
+++ BP/Util/util_functions.cpp
@@ -1,6 +1,39 @@
 #include "util_functions.h"
 
-double softmax(vector<double> values, double temp)
-{
+#include <vector>
+#include <stdlib.h>
+#include <cmath>
 
+using namespace std;
+
+vector<double> softmax(vector<double> values, double temp)
+{
+    double sum = 0.0;
+    for(int i = 0; i < values.size(); i++){
+        values[i] = exp(values[i] / temp);
+        sum += values[i];
+    }
+    for(int i = 0; i < values.size(); i++){
+        values[i] = values[i] / sum;
+    }
+    return values;
+}
+
+int weightedRandomSelection(vector<double> probabilities){
+    double random = rand() / static_cast <double> (RAND_MAX);
+    double currentSum = 0.0;
+
+    for(int i = 0; i < probabilities.size(); i++){
+        currentSum += probabilities[i];
+        if(random < currentSum){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int softmaxActionSelection(vector<double> values, double temp){
+    vector<double> probabilities = softmax(values, temp);
+    return weightedRandomSelection(probabilities);
 }
--- BP/Util/util_functions.h
+++ BP/Util/util_functions.h
@@ -1,11 +1,10 @@
 #ifndef UTIL_FUNCTIONS_H
 #define UTIL_FUNCTIONS_H
 
+#include <vector>
 
-class Util_functions
-{
-public:
-    Util_functions();
-};
+using namespace std;
+
+vector<double> softmax(vector<double> values, double temp);
 
 #endif // UTIL_FUNCTIONS_H
